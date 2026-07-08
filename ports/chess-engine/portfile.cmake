45691d63406ffd2c62705fc4fe76269601df2492
ByteMe6 <tsapiy.yurka@gmail.com>
2026-07-08 22:01:36 +0300

    vcpkg port: v1.1.0 tarball hash

--- ports/chess-engine/portfile.cmake
+++ ports/chess-engine/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ByteMe6/chess-engine
     REF "v${VERSION}"
-    SHA512 1aae58167cf85b62d484ebea4ffda2c5cf19b126d0f43abc7c1366edad95331876a0c1e830dbd433c5ab74c210e784460f8024e189a9ece54f74ec12a5b679e5
+    SHA512 4cf92b26c135cb87c4aff96fb41a2bce71f7f60de6fdf8a67675cd941927a5d1d5be3570cb97afdbe582d02c4375597275cedc42b1cffe83f6fe22fb590c7b9e
     HEAD_REF master
 )
 
